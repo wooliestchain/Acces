c6919ffcbbdeabc3c9abb87bdae29b36be343c01
Emmanuel <levyren38@gmail.com>
2022-11-08 14:51:56 +0100

    Gestion d'accés.

--- users.php
+++ users.php
@@ -1,23 +1,4 @@
-<?php
-    date_default_timezone_set('Africa/Tunis');
-    $d = date("d-m-Y");
 
-    $Tarrive = mktime(9,00,00);
-    $TimeArrive = date("H-i-s",$Tarrive);
-
-    $Tleft = mktime(16,00,00);
-    $Timeleft = date("H:i:sa", $Tleft);
-
-    if (!empty($_POST['seldate'])) {
-        $seldate = $_POST['date'];
-    }
-    else{
-        $seldate = $d;
-    }
-
-    $_SESSION["exportdata"] = $seldate;
-
-?>
 
 <!doctype html>
 <html lang="en">
@@ -27,9 +8,20 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="users.css">
-    <title>Document</title>
+    <title>Utilisateurs</title>
 </head>
 <body>
+    <header>
 
+        <div class="topnav">
+            <a href="main_page.php"><img src="poste.jpg" alt=""></a>
+            <a href="users.php" class="text">Utilisateurs</a>
+            <a href="manage-users.php" class="text">Gestion des utilisateurs</a>
+            <a href="users-log.php" class="text">Entrée des utilisateurs</a>
+            <a class="text">Se deconnecter</a>
+        </div>
+        <div class="up_info1 alert-danger"></div>
+        <div class="up_info2 alert-success"></div>
+    </header>
 </body>
 </html>
