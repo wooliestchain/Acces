4eda0d30a044a54e1aab57df280e735762c57375
Emmanuel <levyren38@gmail.com>
2022-11-11 01:04:23 +0100

    Gestion d'accés.

--- profil.php
+++ profil.php
@@ -21,5 +21,116 @@
         <div class="up_info2 alert-success"></div>
     </header>
 
+    <section>
+        <h1 class="user_list">Informations de l'employé</h1>
+        <div class="table_list">
+            <table class="table">
+                <thead class="table_primary">
+                <tr>
+                    <td class="info">NOM</td>
+                    <td class="info">Numéro</td>
+                    <td class="info">Genre</td>
+                    <td class="info" hidden>UID</td>
+                    <td  class="info" hidden>UID Select</td>
+                </tr>
+                </thead>
+                <tbody class="table_secondary">
+                <?php
+                //Connect to database
+                $nom_serveur = "localhost";
+                $utilisateur = "root";
+                $mot_de_passe = "";
+                $nom_base_données = "nodemculog";
+                $con = mysqli_connect($nom_serveur, $utilisateur, $mot_de_passe, $nom_base_données);
+                $id = $_GET['id'];
+
+
+                $sql = "SELECT * FROM users WHERE id=$id";
+                $result = mysqli_stmt_init($con);
+                if (!mysqli_stmt_prepare($result, $sql)) {
+                    echo '<p class="error">SQL Error</p>';
+                }
+                else{
+                    mysqli_stmt_execute($result);
+                    $resultl = mysqli_stmt_get_result($result);
+                    if (mysqli_num_rows($resultl) > 0){
+                        while ($row = mysqli_fetch_assoc($resultl)){
+                            ?>
+                            <tr>
+                                <td><?php echo $row['username'];?></td>
+                                <td><?php echo $row['SerialNumber'];?></td>
+                                <td><?php echo $row['gender'];?></td>
+                                <td hidden><?php echo $row['CardID'];?></td>
+                                <td hidden><?php echo $row['CardID_select'];?></td>
+                            </tr>
+                            <?php
+                        }
+                    }
+                }
+                ?>
+                </tbody>
+            </table>
+        </div>
+
+        <h1 class="user_list">Historique des entrées</h1>
+        <div class="table_list">
+            <table class="table">
+                <thead class="table_primary">
+                <tr>
+                    <td class="info">Carte</td>
+                    <td class="info">Nom</td>
+                    <td class="info">Numéro</td>
+                    <td class="info">Date</td>
+                    <td class="info">Heure d'entrée</td>
+                    <td class="info">Heure de sortie</td>
+                    <td class="info" >Appréciation</td>
+                    <td  class="info" hidden>Numéro de batiment</td>
+                </tr>
+                </thead>
+                <tbody class="table_secondary">
+                <?php
+                //Connect to database
+                $nom_serveur = "localhost";
+                $utilisateur = "root";
+                $mot_de_passe = "";
+                $nom_base_données = "nodemculog";
+                $con = mysqli_connect($nom_serveur, $utilisateur, $mot_de_passe, $nom_base_données);
+                $username = $_GET['username'];
+
+
+                $sql = "SELECT * FROM logs WHERE username=$username";
+                $result = mysqli_stmt_init($con);
+                if (!mysqli_stmt_prepare($result, $sql)) {
+                    echo '<p class="error">SQL Error</p>';
+                }
+                else{
+                    mysqli_stmt_execute($result);
+                    $resultl = mysqli_stmt_get_result($result);
+                    if (mysqli_num_rows($resultl) > 0){
+                        while ($row = mysqli_fetch_assoc($resultl)){
+                            ?>
+                            <tr>
+                                <td><?php echo $row['CardNumber'];?></td>
+                                <td><?php echo $row['username'];?></td>
+                                <td><?php echo $row['SerialNumber'];?></td>
+                                <td><?php echo $row['DateLog'];?></td>
+                                <td><?php echo $row['TimeIn'];?></td>
+                                <td><?php echo $row['TimeOut'];?></td>
+                                <td><?php echo $row['UserStat'];?></td>
+                                <td hidden><?php echo $row['building_number'];?></td>
+                            </tr>
+                            <?php
+                        }
+                    }
+                }
+                ?>
+                </tbody>
+            </table>
+        </div>
+
+
+
+    </section>
+
 </body>
 </html>
