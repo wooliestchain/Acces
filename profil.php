e4a1e27c2999ccb819919d71035541e03b0a020f
Emmanuel <levyren38@gmail.com>
2022-11-21 20:19:07 +0100

    Gestion d'accés.

--- profil.php
+++ profil.php
@@ -21,57 +21,7 @@
         <div class="up_info2 alert-success"></div>
     </header>
 
-    <section>
-        <h1 class="user_list">Informations de l'employé</h1>
-        <div class="table_list">
-            <table class="table">
-                <thead class="table_primary">
-                <tr>
-                    <td class="info">NOM</td>
-                    <td class="info">Numéro</td>
-                    <td class="info">Genre</td>
-                    <td class="info" hidden>UID</td>
-                    <td  class="info" hidden>UID Select</td>
-                </tr>
-                </thead>
-                <tbody class="table_secondary">
-                <?php
-                //Connect to database
-                $nom_serveur = "localhost";
-                $utilisateur = "root";
-                $mot_de_passe = "";
-                $nom_base_données = "nodemculog";
-                $con = mysqli_connect($nom_serveur, $utilisateur, $mot_de_passe, $nom_base_données);
-                $username = $_GET['username'];
 
-
-
-                $sql = "SELECT * FROM logs WHERE username=$username";
-                $result = mysqli_stmt_init($con);
-                if (!mysqli_stmt_prepare($result, $sql)) {
-                    echo '<p class="error">SQL Error</p>';
-                }
-                else{
-                    mysqli_stmt_execute($result);
-                    $resultl = mysqli_stmt_get_result($result);
-                    if (mysqli_num_rows($resultl) > 0){
-                        while ($row = mysqli_fetch_assoc($resultl)){
-                            ?>
-                            <tr>
-                                <td><?php echo $row['username'];?></td>
-                                <td><?php echo $row['SerialNumber'];?></td>
-                                <td><?php echo $row['gender'];?></td>
-                                <td hidden><?php echo $row['CardID'];?></td>
-                                <td hidden><?php echo $row['CardID_select'];?></td>
-                            </tr>
-                            <?php
-                        }
-                    }
-                }
-                ?>
-                </tbody>
-            </table>
-        </div>
 
         <h1 class="user_list">Historique des entrées</h1>
         <div class="table_list">
